8304bc3900a9d06ae3c72650d9dbd543cb4c7ddc
Denyonepeny <denisshulga1992@gmail.com>
2022-12-31 18:57:32 +0300

    done

---
+++ src/main/java/com/epam/mjc/StudentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.epam.mjc;
+
+public class StudentNotFoundException extends IllegalArgumentException{
+    public StudentNotFoundException(String id) {
+        super(String.format("Could not find student with ID %s", id));
+    }
+}
